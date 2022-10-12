4660c4fd0970024ecc463a1e166ff4f360dd1021
kbleul <kbleulseged@gmail.com>
2022-10-12 18:34:57 -0400

    deploy 2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 
   mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+    app.listen(4000, () => console.log(`Server running on port 4000`))
   })
 
 
