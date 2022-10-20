2fc4335c87b7e731d73b04c6ab063e67475e3db6
kbleul <kbleulseged@gmail.com>
2022-10-20 04:06:23 -0400

    resume download 14

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors");
-import fs from 'fs';
+const fs =  require("fs");
 // const fileRoute = require("./Routes/fileRoutes")
 
 let path = require('path');
